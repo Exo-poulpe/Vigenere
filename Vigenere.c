fad47a94b73f5dab608b4779cb68a4b49b5f067d
poulpe <poulpe@DebPoulpe.home>
2018-10-11 18:06:21 +0200

    First commit

---
+++ Vigenere.c
@@ -0,0 +1,21 @@
+/*
+ * Author : Troller Fabian
+ * Date : 2018.10.11
+ * Description : This program can be crypt by methods vigenere
+ * Version : 0.0.1.0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static char Alphabet[] = {'a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+int main()
+{
+	
+
+    printf("%s",Alphabet);
+    return 0;
+}
+
