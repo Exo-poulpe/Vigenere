660db2d63b60216d74352b4393d4ca0b321260ea
poulpe <poulpe@DebPoulpe.home>
2018-10-22 10:26:02 +0200

    Add select choice for crypt and decrypt

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "Vigenere.h"
+#include "argtable3.h"
+
+
 
 static char pass[50];
 static char txt[100],out[100];
-static char opt[2];
+static char opt[1];
 
 
 void CryptMethods();
@@ -13,24 +16,30 @@ void DecryptMethods();
 
 int main(int argc,char *argv[])
 {
-	printf("Choice option :\nc = Crypt\nd = Decrypt\n");
-	scanf("%s",&opt);
+    printf("Choice option :\nc = Crypt\nd = Decrypt\n");
+    scanf("%s",&opt);
 
-           if(opt == "-c")
-	   {
-		CryptMethods();
-	   } 
-	   else if(opt == "-d")
-	   {
-		DecryptMethods();
-	   }
-           else if(opt == "-Q")
-	   {
 
-	printf("%s/\n",opt);
-	      printf("Only '-c' and '-d' methods for the moments\n");
-	   }
+    while (*opt!='Q')
+        {
+            if(*opt == 'c')
+                {
+                    CryptMethods();
+                }
+            else if(*opt == 'd')
+                {
+                    DecryptMethods();
+                }
+            else if(*opt == 'Q')
+                {
+                    printf("%s/\n",opt);
+                    printf("Only '-c' and '-d' methods for the moments\n");
+                }
+            printf("Choice option :\nc = Crypt\nd = Decrypt\n");
+            scanf("%s",&opt);
 
+
+        }
     return 0;
 }
 
@@ -38,20 +47,22 @@ int main(int argc,char *argv[])
 void CryptMethods()
 {
     printf("Entrer votre Text : \n");
-    fgets(txt,sizeof(txt),stdin);
+    //fgets(txt,sizeof(txt),stdin);
+    scanf("%s",&txt);
     printf("Entrer votre password : ");
     scanf("%s",&pass);
     VigenereCryptMethods(txt,pass,out);
-    printf("Result : \n %s",out);
+    printf("Result : \n============\n%s\n============\n",out);
 
 }
 void DecryptMethods()
 {
     printf("Entrer votre Text : \n");
-    fgets(txt,sizeof(txt),stdin);
+    //fgets(txt,sizeof(txt),stdin);
+    scanf("%s",&txt);
     printf("Entrer votre password : ");
     scanf("%s",&pass);
     VigenereDecryptMethods(txt,pass,out);
-    printf("Result : \n%s",out);
+    printf("Result : \n============\n%s\n============\n",out);
 
 }
