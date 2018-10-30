c18477144e60c4911e99a5d1a2c40a082bb7495d
poulpe <poulpe@DebPoulpe.home>
2018-10-30 18:40:48 +0100

    Version on CLI
    Version use 'unistd.h' lib for parsing argument

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 #include "Vigenere.h"
 
 
 
 static char pass[50];
 static char txt[100],out[100];
-static char opt[1];
 
 
 void CryptMethods();
@@ -15,31 +16,50 @@ void DecryptMethods();
 
 int main(int argc,char *argv[])
 {
-    printf("Choice option :\nc = Crypt\nd = Decrypt\n");
-    scanf("%s",&opt);
+	//extern char *optarg;
+static int password = 0;
+static int flag = 0;
+static int opt;
+enum { WORD_MODE, LINE_MODE } op_mode = WORD_MODE;
 
 
-    while (*opt!='Q')
-        {
-            if(*opt == 'c')
-                {
-                    CryptMethods();
-                }
-            else if(*opt == 'd')
-                {
-                    DecryptMethods();
-                }
-            else if(*opt == 'Q')
-                {
-                    printf("%s/\n",opt);
-                    printf("Only '-c' and '-d' methods for the moments\n");
-                }
-            printf("Choice option :\nc = Crypt\nd = Decrypt\n");
-            scanf("%s",&opt);
-
-
+    while ((opt = getopt(argc, argv, "c:d:p:h")) != -1) {
+        switch (opt)
+       	{
+	case 'p':strcpy(pass,optarg); password=1; break;
+        case 'c':strcpy(txt,optarg); flag=1; break;
+        case 'd':strcpy(txt,optarg); flag=2; break;
+	case 'h': GetHelp(argv[0]); break;
+        default:
+		GetHelp(argv[0]);
+            exit(EXIT_FAILURE);
         }
+
+	if(flag==1 && password==1)
+	{
+		//printf("pass = %s   Word = %s",pass,txt);
+		VigenereCryptMethods(txt,pass,out);	
+		printf("\n%s\n",out);
+	}
+	else if(flag == 2 && password == 1)
+	{
+		VigenereDecryptMethods(txt,pass,out);	
+		printf("\n%s\n",out);
+	}
+
+    }
+	if(argc < 2)
+	{
+		GetHelp(argv[0]);
+	}
+
     return 0;
+}
+
+
+void GetHelp(char fileName[])
+{
+fprintf(stderr, "Usage: %s [-cdh] \n\t-c : crypt with vigenere\n\t-d : decrypt vigenere\n\t-p : password for crypt or decrypt\n\t-h : show help\n", fileName);
 }
 
 
